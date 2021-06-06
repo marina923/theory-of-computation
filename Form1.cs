f4ed61ab6ee6dcc17812c00b4500fdba44a152dd
keroayad1 <66192547+keroayad1@users.noreply.github.com>
2021-06-06 13:16:14 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace RegExp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Regex rgx = new Regex(textBox1.Text);
+            bool result = rgx.IsMatch(textBox2.Text);
+            label3.Text = result.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
